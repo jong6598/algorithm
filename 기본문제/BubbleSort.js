c3e9ae4153478bf8843f0e6cc501ac22c4403e7a
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-21 17:31:38 +0900

    Optimization BubbleSort

--- 기본문제/BubbleSort.js
+++ 기본문제/BubbleSort.js
@@ -1,14 +1,18 @@
 const bubbleSort = (arr) => {
+    let noSwaps;
     for (let i=arr.length; i>0; i--){
-        for (let j=0; j<=i-1; j++){
+        noSwaps = true;
+        for (let j=0; j<i-1; j++){
             //i가 감소할때 마다 j도 줄어들기 때문에 루프의 실행 횟수가 점차 감소함
-            console.log(arr, arr[j], arr[j+1])
+            console.log(arr, arr[j], arr[j+1],noSwaps)
             if(arr[j]>arr[j+1]){
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                noSwaps=false;
             }
         }
+        if (noSwaps) break;
     }
     return arr;
 }
