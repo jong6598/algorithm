5903a85f3265553d667725c27e742758bd16c871
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-24 22:39:46 +0900

    Update InsertionSort.js

--- 기본문제/InsertionSort.js
+++ 기본문제/InsertionSort.js
@@ -1,7 +1,7 @@
 const insertionSort = (arr) =>{
     for (let i=1; i<arr.length; i++){
         let selectVal = arr[i]
-        for (let j=i-1; j>=0&arr[j]>selectVal; j--){
+        for (let j=i-1; j>=0 && arr[j]>selectVal; j--){
            arr[j+1]=arr[j]
         }
         arr[j+1] = selectVal;
